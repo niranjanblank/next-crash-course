6203128bd0352bcc116e98b2caa3bdd4d212d57c
niranjan <niranjanshah474@gmail.com>
2023-04-16 23:57:39 +1000

    Data fetching using getStaticProps in index.tsx and viewing them

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,50 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+interface HomeProp{
+  articles: any
+}
+
+const Home:React.FC<HomeProp> = ({articles}) => {
+ 
+
   return (
   <div>
     <Head>
       <title>Recipe API</title>
       <meta name='keywords' content='web'></meta>
     </Head>
-    <h1>
-      Welcome to recipe API
-    </h1>
+    <div className="flex  flex-col justify-center items-center">
+      {articles.map((article:any)=> {
+        return (
+          <div className="p-4 my-2 rounded-lg w-3/5 border-solid border-2 bg-red-20" key={article.id}>
+            <h3 className="font-bold" >
+              {article.title}
+            </h3>
+            <p>
+              {article.body}
+            </p>
+          </div>
+        )
+      })}
+    </div>
+    
   </div>
   )
 }
+
+// Pre renders a page at build time
+// HTML for the page is generated at build time and served as a static file when the page is requested, rather than being generated on the fly for eaach request
+export const getStaticProps = async() => {
+  const res = await fetch(`https:/jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      // this is passed to the component above
+      articles
+    }
+  }
+}
+
+export default Home
